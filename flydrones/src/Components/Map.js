2b08990152a0c86d2b84df031feefef2e76ef1de
Kyler <kyler.figgins@gmail.com>
2018-03-31 13:36:10 -0600

    added some functions to get current location and zoom.

--- flydrones/src/Components/Map.js
+++ flydrones/src/Components/Map.js
@@ -7,25 +7,28 @@ import {
   Marker
 } from "react-google-maps";
 
-const MyMapComponent = compose(
+const Map = compose(
   withProps({
     googleMapURL:
-      "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyAd3YVr3FLRS5i7igx4jZFuqc5KsAa5nlM&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `400px` }} />,
+    containerElement: <div style={{ height: `400px`, width: `50%` }} />,
     mapElement: <div style={{ height: `100%` }} />
   }),
   withScriptjs,
   withGoogleMap
 )(props => (
-  <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
+  <GoogleMap
+    ref={props.mapLoaded.bind(this)}
+    defaultZoom={props.zoom}
+    defaultCenter={props.center}
+    onDragEnd={props.mapMoved}
+    onZoomChanged={props.mapZoom}
+  >
     {props.isMarkerShown && (
       <Marker
         position={{ lat: -34.397, lng: 150.644 }}
         onClick={props.onMarkerClick}
-        bootstrapURLKeys={{
-          key: "AIzaSyAd3YVr3FLRS5i7igx4jZFuqc5KsAa5nlM"
-        }}
       />
     )}
   </GoogleMap>
@@ -33,7 +36,8 @@ const MyMapComponent = compose(
 
 class MyFancyComponent extends React.PureComponent {
   state = {
-    isMarkerShown: false
+    isMarkerShown: false,
+    map: null
   };
 
   componentDidMount() {
@@ -51,11 +55,30 @@ class MyFancyComponent extends React.PureComponent {
     this.delayedShowMarker();
   };
 
+  mapMoved = () => {
+    console.log("dragged to " + JSON.stringify(this.state.map.getCenter()));
+  };
+
+  mapLoaded = map => {
+    if (this.state.map != null) return;
+
+    this.setState({ map: map });
+  };
+
+  mapZoom = () => {
+    console.log("zoom to " + JSON.stringify(this.state.map.getZoom()));
+  };
+
   render() {
     return (
-      <MyMapComponent
+      <Map
+        center={{ lat: 41.7603785, lng: -111.8480305 }}
+        zoom={15}
         isMarkerShown={this.state.isMarkerShown}
         onMarkerClick={this.handleMarkerClick}
+        mapLoaded={this.mapLoaded}
+        mapMoved={this.mapMoved}
+        mapZoom={this.mapZoom}
       />
     );
   }
